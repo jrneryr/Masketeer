3f79088228b130fcd9df28af2bcfb0d3835b6300
jrneryr <juniorneryr@gmail.com>
2025-04-14 18:10:57 -0300

    Implement structure

---
+++ src/MasketeerWrapper.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MasketeerWrapper: React.FC = () => {
+  return <div>hello world, i am Makheteer</div>;
+};
+
+export default MasketeerWrapper;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as MasketeerWrapper } from "./MasketeerWrapper";
